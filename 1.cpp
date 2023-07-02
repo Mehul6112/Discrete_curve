a71d56a677cfc3c8d36c0216ddf0675920ddb954
Mehul <119481480+Mehul6112@users.noreply.github.com>
2023-07-03 00:19:43 +0530

    version 1.3

--- 1.cpp
+++ 1.cpp
@@ -1,76 +1,75 @@
 #include <iostream>
 #include <set>
-
 using namespace std;
 
-class SET {
+class MySet {
 private:
-  set<int> set;
+  set<int> elements;
 
 public:
-  SET() {}
+  MySet() {}
 
   void add(int value) {
-    set.insert(value);
+    elements.insert(value);
   }
 
   void printPowerSet() {
-    int n = set.size();
+    int n = elements.size();
     set<set<int>> powerSet;
     for (int i = 0; i < (1 << n); i++) {
       set<int> subset;
       for (int j = 0; j < n; j++) {
         if (i & (1 << j)) {
-          subset.insert(set[j]);
+          subset.insert(*next(elements.begin(), j));
         }
       }
       powerSet.insert(subset);
     }
 
-    for (auto subset: powerSet) {
-      for (auto element: subset) {
+    for (auto subset : powerSet) {
+      for (auto element : subset) {
         cout << element << " ";
       }
       cout << endl;
     }
   }
 
-  void printUnion(set<int> s1, set<int> s2) {
+  void printUnion(const set<int>& s1, const set<int>& s2) {
     set<int> unionSet;
-    for (auto element: s1) {
+    for (auto element : s1) {
       unionSet.insert(element);
     }
-    for (auto element: s2) {
+    for (auto element : s2) {
       unionSet.insert(element);
     }
 
-    for (auto element: unionSet) {
+    for (auto element : unionSet) {
       cout << element << " ";
     }
     cout << endl;
   }
 
-  void printIntersection(set<int> s1, set<int> s2) {
+  void printIntersection(const set<int>& s1, const set<int>& s2) {
     set<int> intersectionSet;
-    for (auto element: s1) {
+    for (auto element : s1) {
       if (s2.find(element) != s2.end()) {
         intersectionSet.insert(element);
       }
     }
 
-    for (auto element: intersectionSet) {
+    for (auto element : intersectionSet) {
       cout << element << " ";
     }
     cout << endl;
   }
 
   bool isElementPresent(int element) {
-    return set.find(element) != set.end();
+    return elements.find(element) != elements.end();
   }
 
-  bool isSubset(set<int> subset) {
-    for (auto element: subset) {
-      if (set.find(element) == set.end()) {
+  bool isSubset(const set<int>& subset) {
+    for (auto element : subset) {
+      if (elements.find(element) == elements.end()) {
         return false;
       }
     }
@@ -78,10 +77,10 @@ public:
     return true;
   }
 
-  set<int> getComplement(set<int> universalSet) {
+  set<int> getComplement(const set<int>& universalSet) {
     set<int> complement;
-    for (auto element: universalSet) {
-      if (set.find(element) == set.end()) {
+    for (auto element : universalSet) {
+      if (elements.find(element) == elements.end()) {
         complement.insert(element);
       }
     }
@@ -89,9 +88,9 @@ public:
     return complement;
   }
 
-  set<int> getSetDifference(set<int> s1, set<int> s2) {
+  set<int> getSetDifference(const set<int>& s1, const set<int>& s2) {
     set<int> difference;
-    for (auto element: s1) {
+    for (auto element : s1) {
       if (s2.find(element) == s2.end()) {
         difference.insert(element);
       }
@@ -100,16 +99,16 @@ public:
     return difference;
   }
 
-  set<int> getSymmetricDifference(set<int> s1, set<int> s2) {
+  set<int> getSymmetricDifference(const set<int>& s1, const set<int>& s2) {
     set<int> difference;
 
-    for (auto element: s1) {
+    for (auto element : s1) {
       if (s2.find(element) == s2.end()) {
         difference.insert(element);
       }
     }
 
-    for (auto element: s2) {
+    for (auto element : s2) {
       if (s1.find(element) == s1.end()) {
         difference.insert(element);
       }
@@ -118,58 +117,112 @@ public:
     return difference;
   }
 
-void printCartesianSet(const set<int>& S1, const set<int>& S2) {
-    
-    cout << "Cartesian Product Sets: " << endl;
-    
-    for (const auto& x : S1) {
-        for (const auto& y : S2) {
-            cout << x << " " << y << endl;
-        }
+  void printCartesianSet(const set<int>& s2, const set<int>& s3) {
+
+    for (const auto& x : s2) {
+      for (const auto& y : s3) {
+        cout << x << " " << y << endl;
+      }
     }
     cout << endl;
-    
-} 
+  }
 };
 
 int main() {
-  SET s;
+
+  MySet s;
   s.add(1);
   s.add(2);
   s.add(3);
-
+  set<int> uniset = {1,2,3,4,5,6,7,8,9};
   set<int> s2 = {1,2,3,4};
   set<int> s3 = {0,3,4,6,7};
-  set<int> uniset = {1,2,3,4,5,6,7,8,9};
-    
-  s.printPowerSet();
-
-  s.printUnion(s2, s3);
-  s.printIntersection(s2, s3);
-
-  cout << s.isElementPresent(5) << endl;
-  cout << s.isElementPresent(3) << endl;
-    
   set<int> subset = {1,2};
-  cout << s.isSubset(subset) << endl;
+
+
+    int choice;
+
+  do {
+    cout << "Menu:" << endl;
+    cout << "1. Check if an element is present" << endl;
+    cout << "2. Print power set" << endl;
+    cout << "3. Check if a set is a subset" << endl;
+    cout << "4. Print union & intersection of two sets" << endl;
+    cout << "5. Get complement of a set" << endl;
+    cout << "6. Get set difference & symmetric difference" << endl;
+    cout << "7. Print Cartesian product of two sets" << endl;
+    cout << "8. Quit" << endl;
+    cout << "Enter your choice: ";
+    cin >> choice;
     
-  set<int> complement = s.getComplement(uniset);
+    switch (choice) {
+      case 1: {
+        int element;
+        cout << "Enter the element to check: ";
+        cin >> element;
+        bool present = s.isElementPresent(element);
+        cout << (present ? "Element is present" : "Element is not present") << endl;
+        break;
+      }
+      case 2: {
+        s.printPowerSet();
+        break;
+      }
+      case 3: {
+        bool subsetOfMySet = s.isSubset(subset);
+        cout << (subsetOfMySet ? "The given set is a subset" : "The given set is not a subset") << endl;
+        break;
+      }
+      case 4: {
+        cout << "Union of S2 & S3:"; 
+        s.printUnion(s2, s3);
+        cout <<endl;
+        cout << "Intersection of S2 & S3:"; 
+        s.printIntersection(s2, s3);
+        cout <<endl;
+        break;
+      }
+      case 5: {
+            cout<< "Complement is :" << endl;
+            set<int> complement = s.getComplement(uniset);
 
-  for (auto element: complement) {
-    cout << element << " ";
-  }
-  cout << endl;
+            for (auto element: complement) {
+                cout << element << " ";
+            }
+            cout << endl;
+        break;
+      }
+      case 6: {
+            cout << "Set difference of S2 & S3:" << endl; 
+            set<int> set_difference = s.getSetDifference(s2, s3);
+            for (auto element: set_difference) {
+                cout << element << " ";
+            }
 
-  set<int> difference = s.getSetDifference(s2, s3);
-  for (auto element: difference) {
-    cout << element << " ";
+            cout << "Symmetric set difference of S2 & S3:" << endl; 
+            set<int> sym_set_difference = s.getSymmetricDifference(s2, s3);
+            for (auto element: sym_set_difference) {
+                cout << element << " ";
+            }
+        break;
+      }
+      case 7: {
+        cout << "Cartesian products are:" << endl;
+        s.printCartesianSet(s2, s3);
+        break;
+      }
+      case 8: {
+        cout << "Exiting the program..." << endl;
+        break;
+      }
+      default: {
+        cout << "Invalid choice. Please try again." << endl;
+        break;
+      }
+    }
 
-  set<int> difference = s.getSymmetricDifference(s2, s3);
-  for (auto element: difference) {
-    cout << element << " ";
-
-  printCartesianSet(s2, s3);
-    
+    cout << endl;
+  } while (choice != 8);
 
   return 0;
 }
